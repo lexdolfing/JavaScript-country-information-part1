07a05b57da87a337ef5e77f7947ee014ea1fa3b1
lexdolfing <lexdolfing@gmail.com>
2023-02-16 16:56:33 +0100

    assignment 1 through 5

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
+import axios from 'axios';
 console.log('Hallo daar!');
+
+// Schrijf een asynchrone functie die een GET request maakt naar endpoint /all
+
+async function fetchAllCountries () {
+    try {
+        const result = await axios.get("https://restcountries.com/v2/all");
+        console.log(result.data)
+        console.log(result.data[0].name);
+        const banaan = document.getElementById("first-country")
+        banaan.innerHTML = `
+        <li>${result.data[0].name} </li>
+        <p>Has a population of: ${result.data[0].population} amount people</p>`
+
+
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+fetchAllCountries();
