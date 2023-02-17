fe946300f8d29b9c7949dc1629998c1476656a03
lexdolfing <lexdolfing@gmail.com>
2023-02-17 16:13:35 +0100

    finished the basic assignment

--- dist/index.0641b553.js
+++ dist/index.0641b553.js
@@ -565,12 +565,22 @@ console.log("Hallo daar!");
 async function fetchAllCountries() {
     try {
         const result = await (0, _axiosDefault.default).get("https://restcountries.com/v2/all");
-        console.log(result.data);
-        console.log(result.data[0].name);
+        console.log(result);
+        // Hier maak ik een array van alleen de data van de landen
+        const allCountriesArray = result.data;
+        // Hier sorteer ik die array op basis van populatie van laag naar hoog
+        const allCountriesSortByPop = allCountriesArray.sort((a, b)=>{
+            return a.population - b.population;
+        });
+        // Map functie, waarin ik een array maak met alle html-elementen die geprint moeten worden per land. Elk <li> element heeft een class gebaseerd op de regio van het land, daarom is de aparte functie voor kleuren afwezig.
+        const displayAllCountries = allCountriesSortByPop.map((country, index)=>{
+            return `<li class="${allCountriesSortByPop[index].region}"><img src="${allCountriesSortByPop[index].flag}" class="flag-image"/> ${allCountriesSortByPop[index].name}  </li>
+    <p class="population_paragraph">Has a population of: ${allCountriesSortByPop[index].population} people</p>`;
+        });
+        // Hier maak ik van de ge-mapte array één lange lijst, waarbij de komma's verwijderd worden en er een enter achter elk element komt.
+        const displayAllCountriesJoined = displayAllCountries.join(" <br>");
         const banaan = document.getElementById("first-country");
-        banaan.innerHTML = `
-        <li>${result.data[0].name} </li>
-        <p>Has a population of: ${result.data[0].population} amount people</p>`;
+        banaan.innerHTML = displayAllCountriesJoined;
     } catch (e) {
         console.error(e);
     }
