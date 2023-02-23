79a29ef97392a1239153c5f97bf291a9879404a4
lexdolfing <lexdolfing@gmail.com>
2023-02-23 17:01:33 +0100

    started on assignment

--- dist/index.0641b553.js
+++ dist/index.0641b553.js
@@ -574,8 +574,8 @@ async function fetchAllCountries() {
         });
         // Map functie, waarin ik een array maak met alle html-elementen die geprint moeten worden per land. Elk <li> element heeft een class gebaseerd op de regio van het land, daarom is de aparte functie voor kleuren afwezig.
         const displayAllCountries = allCountriesSortByPop.map((country, index)=>{
-            return `<li class="${allCountriesSortByPop[index].region}"><img src="${allCountriesSortByPop[index].flag}" class="flag-image"/> ${allCountriesSortByPop[index].name}  </li>
-    <p class="population_paragraph">Has a population of: ${allCountriesSortByPop[index].population} people</p>`;
+            return `<article><li class="${allCountriesSortByPop[index].region}"><img src="${allCountriesSortByPop[index].flag}" class="flag-image"/> ${allCountriesSortByPop[index].name}  </li>
+    <p class="population_paragraph">Has a population of: ${allCountriesSortByPop[index].population} people</p></article>`;
         });
         // Hier maak ik van de ge-mapte array één lange lijst, waarbij de komma's verwijderd worden en er een enter achter elk element komt.
         const displayAllCountriesJoined = displayAllCountries.join(" <br>");
@@ -585,7 +585,21 @@ async function fetchAllCountries() {
         console.error(e);
     }
 }
-fetchAllCountries();
+// fetchAllCountries();
+async function fetchOneCountry(country) {
+    // BONUS check of de naam van het opgegeven land in de lijst voorkomt
+    try {
+        // maak een string van de url met de naam van het land
+        const url = "https://restcountries.com/v2/name/" + country;
+        console.log(url);
+        // doe een get request met die url
+        const thisCountry = await (0, _axiosDefault.default).get(url);
+        console.log(thisCountry.data[0].name);
+    } catch (e) {
+        console.error(e);
+    }
+}
+console.log(fetchOneCountry("Afghanistan"));
 
 },{"axios":"jo6P5","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"jo6P5":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
--- src/app.js
+++ src/app.js
@@ -15,8 +15,8 @@ async function fetchAllCountries () {
         });
         // Map functie, waarin ik een array maak met alle html-elementen die geprint moeten worden per land. Elk <li> element heeft een class gebaseerd op de regio van het land, daarom is de aparte functie voor kleuren afwezig.
         const displayAllCountries = allCountriesSortByPop.map((country, index) => {
-            return `<li class="${allCountriesSortByPop[index].region}"><img src="${allCountriesSortByPop[index].flag}" class="flag-image"/> ${allCountriesSortByPop[index].name}  </li>
-    <p class="population_paragraph">Has a population of: ${allCountriesSortByPop[index].population} people</p>`
+            return `<article><li class="${allCountriesSortByPop[index].region}"><img src="${allCountriesSortByPop[index].flag}" class="flag-image"/> ${allCountriesSortByPop[index].name}  </li>
+    <p class="population_paragraph">Has a population of: ${allCountriesSortByPop[index].population} people</p></article>`
         })
         // Hier maak ik van de ge-mapte array één lange lijst, waarbij de komma's verwijderd worden en er een enter achter elk element komt.
         const displayAllCountriesJoined = displayAllCountries.join(" <br>")
@@ -29,7 +29,24 @@ async function fetchAllCountries () {
     }
 }
 
-fetchAllCountries();
+// fetchAllCountries();
+
+async function fetchOneCountry(country) {
+    // BONUS check of de naam van het opgegeven land in de lijst voorkomt
+    try {
+        // maak een string van de url met de naam van het land
+        const url = "https://restcountries.com/v2/name/" + country
+        console.log(url)
+        // doe een get request met die url
+        const thisCountry = await axios.get(url);
+        console.log(thisCountry.data[0].name);
+    } catch (e) {
+        console.error(e);
+
+    }
+}
+
+console.log(fetchOneCountry("Afghanistan"))
 
 
 
